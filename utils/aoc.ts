b67065d57b6953f7722105cc14db7cadaeb491d4
Gabriel Toschi <gabriel.toschi@hotmail.com>
2021-12-11 11:52:37 -0300

    New util function: get digit matrix from input

--- utils/aoc.ts
+++ utils/aoc.ts
@@ -6,6 +6,11 @@ export function ints(data: string, separator = '\n'): number[] {
   return data.split(separator).map(line => parseInt(line));
 }
 
+export function digits(data: string, lineSeparator = '\n'): number[][] {
+  return lines(data, lineSeparator)
+    .map(line => line.split('').map(char => parseInt(char)));
+}
+
 export function answer(star: 1 | 2, answer: any) {
   console.log(`Star #0${star}: ${answer.toString()}`);
 }
